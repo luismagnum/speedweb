bfc537a5934b28af49da0cff13d0ca5d817f899a
luismagnum <luismagnum1@gmail.com>
2023-03-31 23:45:06 -0300

    icono whatsapp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
       <div>
         <h1 className='text-teal-400 text-3xl font-semibold sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none'>Contactame Ya..</h1>
         <button className=" duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full">
-        <a href="https://wa.me/542984792639?text=Hola quiero un presupuesto " target="_blank">  
+        <a href="https://wa.me/542984417477?text=Hola quiero un presupuesto " target="_blank">  
         <img src={whatsap} alt="what" width="50px" height="50px" />
         </a>
         </button>
@@ -22,7 +22,9 @@ const Footer = () => {
       <span>@ 2023 Developers Web Luis E. diaz</span>
       <span>SpeedWeb</span>
       <SocialIcons Icons={Icons}/>
-      <a className='text-center text-teal-400 text-2xl ' href='#Seccion1'>HOME</a>
+      <button>
+      <a className='text-center text-teal-400 text-2xl slide-top' href='#Seccion1'>HOME</a>
+      </button>
       </div>
   </footer>
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import speed from '../assets/imagenes/speed.png';
+import whatsap from '../assets/imagenes/whatsapp-fill.svg';
 
-const Hero = () => {
+const Hero = ()=>{
   return (
     <section className='py-4 md:px-24 px-4 fondo'>
     <div className='flex flex-wrap md:flex-row flex-col gap-5 pt-20 container'>
+    <a href="https://wa.me/542984417477?text=Hola quiero un presupuesto " target="_blank" className='fixed  mt-52'>  
+        <img src={whatsap} alt="what" width="60px" height="60px" />
+        </a>
         <div className='flex-1'>
            <h1 className='font-Poppins text-orange-400 md:text-5xl text-4xl font-semibold tracking-wide md:leading-tight leading-snug'>¡Te ayudamos a impulsar tu negocio!</h1>
            <p className='text-orange-400 text-3xl md:w-2/3 md:py-4 py-2 leading-relaxed'>Una Web para alcanzar tus logros.</p>
-        <div className='flex-1 flex justify-center'>
-           <img src={speed} alt='' className='flex items-center justify-center w-50% rounded-xl'/>
+        <div className='flex-1 flex justify-center '>
+           <img src={speed} alt='' className='flex items-center justify-center w-50% '/>
         </div>
         </div>
       
