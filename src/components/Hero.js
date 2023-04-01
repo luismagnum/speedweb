e250cb2f3e71a06e113521fa8c2eec8626f537a8
luismagnum <luismagnum1@gmail.com>
2023-04-01 00:46:52 -0300

    arreglar fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,13 +6,14 @@ const Hero = ()=>{
   return (
     <section className='py-4 md:px-24 px-4 fondo'>
     <div className='flex flex-wrap md:flex-row flex-col gap-5 pt-20 container'>
-    <div className='sticky'>
-    <a href="https://wa.me/542984417477?text=Hola quiero un presupuesto " target="_blank">  
-        <img className='fixed mt-56'src={whatsap} alt="what" width="60px" height="60px" />
-        </a>
-    </div>
         <div className='flex-1'>
            <h1 className='font-Poppins text-orange-400 md:text-5xl text-4xl font-semibold tracking-wide md:leading-tight leading-snug'>¡Te ayudamos a impulsar tu negocio!</h1>
+           <div className='sticky'>
+    <a href="https://wa.me/542984417477?text=Hola quiero un presupuesto " target="_blank">  
+        <img src={whatsap} alt="what" width="60px" height="60px" />
+        </a>
+    </div>
+
            <p className='text-orange-400 text-3xl md:w-2/3 md:py-4 py-2 leading-relaxed'>Una Web para alcanzar tus logros.</p>
         <div className='flex-1 flex justify-center '>
            <img src={speed} alt='' className='flex items-center justify-center w-50% '/>
