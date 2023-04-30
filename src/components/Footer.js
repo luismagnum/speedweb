11a9c3f03466ef12d2e110975eb0192aa53b40e7
luismagnum <luismagnum1@gmail.com>
2023-04-30 00:50:05 -0300

    agregar imagen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import whatsap from '../assets/imagenes/whatsapp-fill.svg';
 const Footer = () => {
   return <footer className='bg-gray-900 text-white' id='Seccion4'>
     <div className='md:flex md:justify-between md: items-center sm:px-12 px-4 bg-[#ffffff19] py-7'>
-      <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5'><span className='text-teal-400'>SpeedWeb</span>Te gustaria empezar un proyecto con nosotros?</h1>
+      <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5'><span className='text-teal-400 mr-3'>SpeedWeb</span>Te gustaria empezar un proyecto con nosotros?</h1>
       <div>
         <h1 className='text-teal-400 text-3xl font-semibold sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none'>Contactame Ya..</h1>
         <button className=" duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full">
@@ -22,7 +22,7 @@ const Footer = () => {
       <span>@ 2023 Developers Web Luis E. diaz</span>
       <span>SpeedWeb</span>
       <SocialIcons Icons={Icons}/>
-      <button>
+      <button className='grid grid-cols-1'>
       <a className='text-center text-teal-400 font-semibold text-2xl hover:text-teal-600' href='#Seccion1'>HOME</a>
       </button>
       </div>
--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import quese from '../assets/imagenes/quese.png';
 import sabor from '../assets/imagenes/sabor.png';
 import speed from '../assets/imagenes/speed.png';
+import logospeed from '../assets/imagenes/logospeed.png';
 
 const Proyects = () => {
   return (
@@ -10,10 +11,11 @@ const Proyects = () => {
     <div className='bg-[#011d41] text-white flex items-center justify-center '>
         <div className='text-white max max-w-5xl'>
             <div className='mt-10 mb-10 h-100 flex items-center justify-between '>
-                <div className='grid grid-cols-1 ml-2 odd:md:grid-cols-3 lg:grid-cols-1 '>
+                <div className='grid grid-cols-1 mt-2 ml-2 odd:md:grid-cols-4 lg:grid-cols-1 '>
                     <img src={sabor} alt='' className='w-48 h-48 ml-6'/>
-                    <img src={speed} alt='' className='w-56 h-56 mt-0 ml-0'/>
-                    <img src={quese} alt='' className='w-44 h-42 rounded-lg mt-8 ml-8 '/>
+                    <img src={logospeed} alt='' className='w-48 h-48 mt-2 ml-6'/>
+                    <img src={speed} alt='' className='w-56 h-56 mt-0 mb-2 ml-0'/>
+                    <img src={quese} alt='' className='w-44 h-42 rounded-lg mt-12 ml-6 '/>
                 </div>
             </div>
 
