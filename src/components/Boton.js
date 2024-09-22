42bd8d234ce6df4023d124ecb5c6f8e9bcd01df7
luismagnum <luismagnum1@gmail.com>
2024-09-22 17:13:48 -0300

    boton

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -4,7 +4,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 const Boton = () => {
   return (
     <a
-      href="https://wa.me/542984792639?text=Hola quiero hacer una consulta" 
+      href="https://wa.me/542984417477?text=Quiero hacer una consulta SpeedWeb" 
       className="fixed bottom-5 right-8 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50"
       target="_blank"
       rel="noopener noreferrer"
