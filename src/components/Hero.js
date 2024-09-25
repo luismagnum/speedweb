46ff30a19943c53afee37e3f10b329b0289b1e78
luismagnum <luismagnum1@gmail.com>
2024-09-24 22:34:52 -0300

    font

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,13 +5,13 @@ const Hero = ()=>{
     <section className='py-4 md:px-24 px-4 fondo'>
     <div className='flex flex-wrap md:flex-row flex-col gap-5 pt-24 container'>
         <div className='flex-1'>
-           <h1 className='font-Poppins text-orange-600 sm:text-2xl md:text-5xl text-4xl font-semibold tracking-wide md:leading-tight leading-snug'>Diseño web ventas online</h1>
+           <h1 className='cabin text-orange-600 sm:text-2xl md:text-5xl text-4xl font-semibold tracking-wide md:leading-tight leading-snug'>Diseño web ventas online</h1>
            <div className='sticky'>
             </div>
-           <p className='font-Poppins text-white text-3xl md:w-2/3 md:py-2 py-2 leading-relaxed'>Nos dedicamos exclusivamente al ecommerce</p>
-           <p className='font-Poppins text-white text-3xl md:w-2/3 md:py-2 py-2 leading-relaxed'>Te ayudamos a que tu negocio crezca</p>
+           <p className='cabin text-white text-3xl md:w-2/3 md:py-2 py-2 leading-relaxed'>Nos dedicamos exclusivamente al ecommerce</p>
+           <p className='cabin text-white text-3xl md:w-2/3 md:py-2 py-2 leading-relaxed'>Te ayudamos a que tu negocio crezca</p>
         <div className='flex-1 flex justify-center items-center'>
-          <p className='text-xl md:w-1/3 h-80 mt-1 font-Poppins text-orange-500 md:text-3xl font-bold'>"Soluciones a tu medida"</p>
+          <p className='text-xl md:w-1/3 h-80 mt-1 cabin text-orange-600 md:text-3xl font-extrabold'>"Soluciones a tu medida"</p>
         </div>
         </div>
     </div>
